41d78250f3532a9269363650960ff6ef0d15e575
NamanHimmatramka <91754816+NamanHimmatramka@users.noreply.github.com>
2023-04-04 23:09:55 +0530

    Delete spring-boot-jdbc-mybatis directory

--- spring-boot-jdbc-mybatis/src/main/java/org/spring/springboot/mybatis/service/TestService.java
+++
@@ -1,8 +0,0 @@
-package org.spring.springboot.mybatis.service;
-
-import org.spring.springboot.mybatis.model.Test;
-
-public interface TestService {
-
-    Test getTestById(String id);
-}
--- spring-boot-jdbc-mybatis/src/main/java/org/spring/springboot/mybatis/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package org.spring.springboot.mybatis.service;
-
-import org.spring.springboot.mybatis.model.User;
-
-public interface UserService {
-
-    User getUserById(String id);
-}
